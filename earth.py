a253efb0d0a4310fc28ed784b5ba9d8a75b3f2f5
faustodamico <fdamico2@mail.austral.edu.ar>
2025-04-04 09:37:58 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,13 +1,11 @@
-x = "Bangladesh"
-y = "Barbados"
-
-if x<y:
-    a="true"
-    b="false"
-else:
-    b="true"
-    a="false"
-
-print("The result of ",x," comes first in the dictionary than ",y," is", a)
-print("The result of ",y," comes first in the dictionary than ",x," is",b)
-
+def earth():    
+    x = "Bangladesh"    
+    y = "Barbados"
+    if x<y:
+        a="true"
+        b="false"
+    else:
+        b="true"
+        a="false"
+    print("The result of ",x," comes first in the dictionary than ",y," is", a)
+    print("The result of ",y," comes first in the dictionary than ",x," is",b)
