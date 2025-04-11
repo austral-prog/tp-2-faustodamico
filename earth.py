41a15c99fcc829e02ec8c49a6dcea9de253a345b
faustodamico <fdamico2@mail.austral.edu.ar>
2025-04-11 10:24:42 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,12 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    if x < y:
-        a = "True"
-        b = "False"
-    else:
-        a = "False"
-        b = "True"
-
+    a = x < y 
+    b = y < x
     print(f"The result of {x} comes first in the dictionary than {y} is {a}.")
     print(f"The result of {y} comes first in the dictionary than {x} is {b}.")
